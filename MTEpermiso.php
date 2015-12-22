3bdd454023f60b91718f0aa94d5a4f77e70f718f
Victor Morales <vic.mor.593@gmail.com>
2015-12-22 14:08:13 -0600

    Se agrego mantenimiento de permisos

---
+++ MTEpermiso.php
@@ -0,0 +1,14 @@
+<form name="formPermiso" method="post">
+  
+  <table>
+    
+    <tr>
+      <div class="input-group">
+        <span class="input-group-addon" id="basic-addon1">ID</span>
+        <input name="idPermiso" type="text" class="form-control" placeholder="Código Unico" aria-describedby="basic-addon1">
+      </div>
+    </tr>
+
+  </table>
+
+</form>
